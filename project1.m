bec08107ebddab7cf70d2e338281521b8b5e28da
tsuiglen <57470225+tsuiglen@users.noreply.github.com>
2023-01-31 07:49:37 -0500

    Update project1.m

--- project1.m
+++ project1.m
@@ -1,17 +1,36 @@
 format long
-n = 4;
+for n = 1:100000000
+    parallel_processor_1 = quarterSum(1,floor(n/4));
+    parallel_processor_2 = quarterSum(floor(n/4)+1,floor(n/2));
+    parallel_processor_3 = quarterSum(floor(n/2)+1,floor(3*n/4));
+    parallel_processor_4 = quarterSum(floor(3*n/4)+1,floor(n));
 
-quarter = n/4;
-half = n/2;
-three_quarters = 3*n/4;
-parallel_processor_1 = quarterSum(1,n/4);
-parallel_processor_2 = quarterSum((n/4)+1,n/2);
-parallel_processor_3 = quarterSum((n/2)+1,3*n/4);
-parallel_processor_4 = quarterSum((3*n/4)+1,n);
-
-total = parallel_processor_1 + parallel_processor_2 + parallel_processor_3 + parallel_processor_4;
-log(2)
+    total = parallel_processor_1 + parallel_processor_2 + parallel_processor_3 + parallel_processor_4;
+    error = abs(log(2)-total);
+    if(n<=300)
+        plot(n,error,'--.'); hold on
+    elseif (n<=10000)
+        if(mod(n,1000)==0)
+            plot(n,error,'--.'); hold on
+        end
+    elseif (n<=1000000)
+        if(mod(n,100000)==0)
+            plot(n,error,'--.'); hold on
+        end
+    elseif (n<=1000000000)
+        if(mod(n,100000000)==0)
+            plot(n,error,'--.'); hold on
+        end
+    end
+end
+figure(1);
+title('Numerical Error vs. Number of Terms');
+xlabel('Number of Terms N');
+ylabel('Numerical Error');
+single(log(2))
 single(total)
+log(2)
+total
 
 function sum = quarterSum(start,finish)
     sum = 0.0;
